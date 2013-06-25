b2f496534f9f6bb245bd78463a067cde79e234ac
Jordi Llonch <llonchj@gmail.com>
2013-06-25 15:03:21 +1000

    Bumped version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'llonchj@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures hypertable'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w(ubuntu).each do |sup|
   supports sup
