ac5d9799f65e31bdfd2532a5e4f54f01f7b0f9b5
Jordi Llonch <llonchj@gmail.com>
2013-07-19 11:15:18 +1000

    config adjustment

--- attributes/default.rb
+++ attributes/default.rb
@@ -36,8 +36,7 @@ default[:hypertable][:conf]["ThriftBroker.Port"] = 38080
 
 #default[:hypertable][:conf]["Hypertable.Logging.Level"] = "info"
 #default[:hypertable][:conf]["Hypertable.Network.Interface"] = nil
-## Hypertable.RangeServer.Range.RowSize.Unlimited=true
-## Hyperspace.Client.Datagram.SendPort=38049
+#default[:hypertable][:conf]["Hypertable.RangeServer.Range.RowSize.Unlimited"]=true
 #default[:hypertable][:conf]["Hyperspace.Client.Datagram.SendPort"] = 38049
 
 
